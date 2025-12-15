b5b12c269134446583f120b8b76e33131d1d031d
[adityarn] <[adityarizkiawann@gmail.com]>
2025-12-15 09:45:10 +0700

    Latihan Pindah Activity - Mengirim data Int

---
+++ app/src/main/java/com/example/myapplication/DetailActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val DATA_ANGKA = "data_angka"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detail)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val tvDataAngka : TextView = findViewById(R.id.tvAngka)
+
+        val angka = intent.getIntExtra(DATA_ANGKA, 0)
+        val textAngka = "Angka : $angka"
+        tvDataAngka.text = textAngka
+
+    }
+}
