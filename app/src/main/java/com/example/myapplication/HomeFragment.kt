e2ac9c7c0945722c3974414bbc311fdf87f61d2d
[adityarn] <[adityarizkiawann@gmail.com]>
2025-12-27 00:44:08 +0700

    Latihan Fragment Dasar

---
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.myapplication.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentHomeBinding.bind(view)
+
+        binding.btnKlik.setOnClickListener {
+            Toast.makeText(requireContext(), "Button di Fragment", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
