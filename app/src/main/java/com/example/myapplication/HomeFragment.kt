7214e076370af8d55de33ca0516395c6cd487328
[adityarn] <[adityarizkiawann@gmail.com]>
2026-01-03 17:49:12 +0700

    update fragment & navhost

--- app/src/main/java/com/example/myapplication/HomeFragment.kt
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.myapplication.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -22,6 +23,11 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tvHomeFragment.setOnClickListener {
+            findNavController()
+                .navigate(R.id.action_home_to_detail)
+        }
     }
 
 }
