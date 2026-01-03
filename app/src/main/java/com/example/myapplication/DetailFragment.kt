cc254b82f540f7486dbf9f9b39f508bc0cbf06eb
[adityarn] <[adityarizkiawann@gmail.com]>
2026-01-04 01:05:21 +0700

    add a few fragment, update code activity_main & res/menu

--- app/src/main/java/com/example/myapplication/DetailFragment.kt
+++ app/src/main/java/com/example/myapplication/DetailFragment.kt
@@ -5,37 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-class HomeFragment : Fragment() {
+class DetailFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.btnDetail).setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, DetailFragment())
-                .addToBackStack(null)
-                .commit()
-        }
     }
 
     override fun onResume() {
         super.onResume()
         val act = requireActivity() as AppCompatActivity
         act.supportActionBar?.apply {
-            title = "Home"
-            setDisplayHomeAsUpEnabled(false)
+            title = "Detail"
+            setDisplayHomeAsUpEnabled(true)
         }
     }
+
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -26,5 +28,42 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Load fragment pertama
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, HomeFragment())
+                .commit()
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId) {
+            R.id.action_profile -> {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, DetailFragment())
+                    .addToBackStack(null)
+                    .commit()
+                true
+            }
+
+            R.id.action_settings -> {
+                Toast.makeText(this, "Settings Clicked", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onSupportNavigateUp() : Boolean {
+        onBackPressedDispatcher.onBackPressed()
+        return true
     }
 }
