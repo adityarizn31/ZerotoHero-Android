33dfa249130907544bb0a58735798a25e1e149f5
[adityarn] <[adityarizkiawann@gmail.com]>
2026-01-14 21:09:50 +0700

    Success NavHostGraph2

---
+++ app/src/main/java/com/example/myapplication/CategoryFragment.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
+import com.example.myapplication.databinding.FragmentCategoryBinding
+import com.example.myapplication.databinding.FragmentHomeBinding
+
+class CategoryFragment : Fragment() {
+
+    private var _binding : FragmentCategoryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCategoryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnHomeFragment.setOnClickListener{
+            findNavController().navigate(R.id.action_categoryFragment_to_homeFragment)
+        }
+    }
+
+
+}
