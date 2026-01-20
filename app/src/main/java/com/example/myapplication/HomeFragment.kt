8980e2ed697380d0a7e87889f890929054ddf571
[adityarn] <[adityarizkiawann@gmail.com]>
2026-01-21 06:17:36 +0700

    example thread statue wrong

---
+++ app/src/main/java/com/example/myapplication/HomeFragment.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding : FragmentHomeBinding ?= null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnStart.setOnClickListener {
+            binding.txtStatus.text = "Sedang Bekerja"
+            Thread.sleep(5000)
+            binding.txtStatus.text = "Selesai !!!"
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
