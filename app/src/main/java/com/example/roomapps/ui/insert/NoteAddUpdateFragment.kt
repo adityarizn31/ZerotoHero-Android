83ff3e9ba60d5e45e785d3664ab5848bbccccf44
[adityarn] <[adityarizkiawann@gmail.com]>
2026-03-08 15:50:30 +0700

    completed codes in NoteAddUpdateFragment

--- app/src/main/java/com/example/roomapps/ui/insert/NoteAddUpdateFragment.kt
+++ app/src/main/java/com/example/roomapps/ui/insert/NoteAddUpdateFragment.kt
@@ -1,12 +1,17 @@
 package com.example.roomapps.ui.insert
 
+import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.lifecycle.ViewModelProvider
 import com.example.roomapps.R
 import com.example.roomapps.database.Note
@@ -16,24 +21,24 @@ import com.example.roomapps.helper.ViewModelFactory
 
 class NoteAddUpdateFragment : Fragment() {
 
-    private var _binding : FragmentNoteAddUpdateBinding ?= null
-    private val binding get() = _binding !!
+    private var _binding: FragmentNoteAddUpdateBinding? = null
+    private val binding get() = _binding!!
 
-    lateinit var noteAddUpdateViewModel : NoteAddUpdateViewModel
+    private lateinit var noteAddUpdateViewModel: NoteAddUpdateViewModel
 
     companion object {
         const val EXTRA_NOTE = "extra_note"
         const val ALERT_DIALOG_CLOSE = 10
-        const val ALERT_DIALOG_DELETE = 10
+        const val ALERT_DIALOG_DELETE = 20
     }
 
     private var isEdit = false
-    private var note : Note?= null
+    private var note: Note? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentNoteAddUpdateBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -51,17 +56,18 @@ class NoteAddUpdateFragment : Fragment() {
             note = Note()
         }
 
-        val actionBarTitle : String
-        val btnTitle : String
+        val actionBarTitle: String
+        val btnTitle: String
 
         if (isEdit) {
             actionBarTitle = getString(R.string.change)
             btnTitle = getString(R.string.update)
 
-            note?.let { note ->
-                binding.edtTitle.setText(note.title)
-                binding.edtDescription.setText(note.description)
+            note?.let {
+                binding.edtTitle.setText(it.title)
+                binding.edtDescription.setText(it.description)
             }
+
         } else {
             actionBarTitle = getString(R.string.add)
             btnTitle = getString(R.string.save)
@@ -70,21 +76,23 @@ class NoteAddUpdateFragment : Fragment() {
         activity?.title = actionBarTitle
         binding.btnSubmit.text = btnTitle
 
-        binding?.btnSubmit?.setOnClickListener {
-            val title = binding?.edtTitle?.text.toString    ().trim()
-            val description = binding?.edtDescription?.text.toString().trim()
+        binding.btnSubmit.setOnClickListener {
+
+            val title = binding.edtTitle.text.toString().trim()
+            val description = binding.edtDescription.text.toString().trim()
 
             when {
 
                 title.isEmpty() -> {
-                    binding?.edtTitle?.error = getString(R.string.empty)
+                    binding.edtTitle.error = getString(R.string.empty)
                 }
 
                 description.isEmpty() -> {
-                    binding?.edtDescription?.error = getString(R.string.empty)
+                    binding.edtDescription.error = getString(R.string.empty)
                 }
 
                 else -> {
+
                     note?.let {
                         it.title = title
                         it.description = description
@@ -103,20 +111,101 @@ class NoteAddUpdateFragment : Fragment() {
                 }
             }
         }
+
+        // Handle tombol back HP
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    showAlertDialog(ALERT_DIALOG_CLOSE)
+                }
+            }
+        )
+
+        // Aktifkan menu di Fragment
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        if (isEdit) {
+            inflater.inflate(R.menu.main_menu, menu)
+        }
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+
+            R.id.acDelete -> {
+                showAlertDialog(ALERT_DIALOG_DELETE)
+            }
+
+            android.R.id.home -> {
+                showAlertDialog(ALERT_DIALOG_CLOSE)
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showAlertDialog(type: Int) {
+
+        val isDialogClose = type == ALERT_DIALOG_CLOSE
+
+        val dialogTitle: String
+        val dialogMessage: String
+
+        if (isDialogClose) {
+
+            dialogTitle = getString(R.string.cancel)
+            dialogMessage = getString(R.string.message_cancel)
+
+        } else {
+
+            dialogTitle = getString(R.string.delete)
+            dialogMessage = getString(R.string.message_delete)
+        }
+
+        val alertDialogBuilder = AlertDialog.Builder(requireContext())
+
+        with(alertDialogBuilder) {
+
+            setTitle(dialogTitle)
+            setMessage(dialogMessage)
+            setCancelable(false)
+
+            setPositiveButton(getString(R.string.yes)) { _, _ ->
+
+                if (!isDialogClose) {
+
+                    noteAddUpdateViewModel.delete(note as Note)
+                    showToast(getString(R.string.deleted))
+                }
+
+                requireActivity().onBackPressedDispatcher.onBackPressed()
+            }
+
+            setNegativeButton(getString(R.string.no)) { dialog, _ ->
+                dialog.cancel()
+            }
+        }
+
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
 
     private fun showToast(message: String) {
         Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 
-    private fun obtainViewModel(requireContext: Context): NoteAddUpdateViewModel {
+    private fun obtainViewModel(context: Context): NoteAddUpdateViewModel {
         val factory = ViewModelFactory.getInstance(requireActivity().application)
-        return ViewModelProvider(this, factory).get(NoteAddUpdateViewModel::class.java)
+        return ViewModelProvider(this, factory)[NoteAddUpdateViewModel::class.java]
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-
 }
